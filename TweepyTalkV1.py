5915d4a84b20d3d94eb2d596b654533fa075e05b
Tze Chien Chu <tzechienchu@gmail.com>
2013-02-26 02:09:53 +0000

    Start use Git to track project Not include AdaFruit python code

---
+++ TweepyTalkV1.py
@@ -0,0 +1,101 @@
+﻿from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import tweepy
+import subprocess
+
+import time
+import datetime
+
+import googlespeech
+import dbaccess
+import tweetprocess
+
+from Adafruit_I2C import Adafruit_I2C
+from Adafruit_MCP230xx import Adafruit_MCP230XX
+import smbus
+import Adafruit_CharLCDPlate
+
+consumer_key = '9PGxxFJTdvSYZ4b8orblUQ'
+consumer_secret = 'qbgxpEsVycW7f8RMnq8HfJ41sVnTedkKdja429tH4'
+access_token = '1146965486-CaI4Khqo8ihl2FqrVuEqFYkcA6qkJCn4xK2RLtI'
+access_token_secret = 'QHhniZqnHdCGIt9ZzSgX2RUqxLb0M4Oq9EqRiSxI'
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets are the received from the stream. 
+    This is a basic listener that just prints received tweets to stdout.
+
+    """
+    def on_status(self, status):
+        print (u"{text}".format(text=status.text))
+        print (u"{name}({screen})\n{created} via {src}\n".format(
+                name = status.author.name,screen=status.author.screen_name,
+                created=status.created_at,src=status.source))
+            
+        userid = status.author.screen_name
+
+        tweettext = tweetprocess.removeHTTPURL(status.text)
+        tweetcommand = tweetprocess.getTweetCommand(tweettext)
+        print tweetcommand
+
+        if tweetcommand == 'talkZH:':
+            speaktext = tweetprocess.processUnicodeforJPZH(tweettext)
+            print speaktext
+            googlespeech.speakSpeechFromTextZH(speaktext)
+        if tweetcommand == 'talkJP:':
+            speaktext = tweetprocess.processUnicodeforJPZH(tweettext)
+            print speaktext
+            googlespeech.speakSpeechFromTextJP(speaktext)
+        if tweetcommand == 'talk:':
+            speaktext = tweetprocess.processUnicodeforEN(tweettext)
+            print speaktext
+            googlespeech.speakSpeechFromTextEN(speaktext)
+            
+        if tweetcommand == 'take:':
+            now = time.time()
+            nowlog = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S')
+            api = tweepy.API(auth)
+            pid = subprocess.call(["fswebcam","test.jpg"])
+            fn = '/home/pi/talkingtweet/test.jpg'
+            api.status_update_with_media(fn,status=nowlog)
+            
+        if tweetcommand == 'show:':
+            showtext = tweetprocess.processUnicodeforEN(tweettext)
+            now = time.time()
+            nowlog = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S')            
+            lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate(busnum = 1)
+            lcd.clear()
+            message = nowlog + '\n'+showtext
+            lcd.message(message)
+    
+            
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    lcd = Adafruit_CharLCDPlate.Adafruit_CharLCDPlate(busnum = 1)
+    lcd.clear()
+    lcd.message("Aloha !\nRPiTalkingTweet")
+    
+    tdb = dbaccess.TWdbaccess("tweetdb.db")
+    userlang = tdb.readUserState()
+    print userlang
+
+##
+##    text = u"RT @nhk_news: 朱莉安 ABCD 不要看電視 http://t.co/wlINNXkl #nhk_news"
+##    speaktext = tweetprocess.removeHTTPURL(text)
+##    print speaktext
+##    speaktext = tweetprocess.processUnicodeforJPZH(speaktext)
+##    print speaktext
+##    googlespeech.speakSpeechFromTextZH(speaktext)
+    
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    stream = Stream(auth, l)	
+    stream.userstream()
+    
---
+++ tweetprocess.py
@@ -0,0 +1,82 @@
+#
+#Twitter Data Process
+#
+
+import re
+import unicodedata
+
+def getTweetCommand(text):
+    rtext = ""
+    if isinstance(text,str):
+        print text
+        text = unicode(text,'unicode-escape')
+    for chr in text:
+        chrtype = unicodedata.category(chr)
+        if chrtype == 'Ll':
+            rtext = rtext + chr
+        if chr == ':':
+            return rtext+':'
+
+def processUnicodeforEN(text):
+    rtext = ""
+    if isinstance(text,str):
+        print text
+        text = unicode(text,'unicode-escape')    
+    for chr in text:
+        chrtype = unicodedata.category(chr)
+        if chrtype == 'Ll' :
+            rtext = rtext + chr
+        if chrtype == 'Lu' :
+            rtext = rtext + chr
+        if chrtype == 'Zs' :
+            rtext = rtext + ' '
+        if chr == ':' :
+            rtext = rtext + ':'            
+            
+    if "show:" in rtext:
+        rtext = rtext.replace("show:","")
+        
+    return rtext
+        
+def processUnicodeforJPZH(text):
+    rtext = u""
+    for chr in text:
+        chrtype = unicodedata.category(chr)
+        if chrtype == 'Lo' :
+            rtext = rtext + chr
+        if chrtype == 'Zs' :
+            rtext = rtext + chr
+            
+    return rtext
+        
+def getHTTPURL(subject):
+    t = ""
+    t = subject[subject.find("http://"):]
+    t = t[0:20]
+    if len(t) < 5:
+        return ""
+    else:
+        return t
+
+def removeHTTPURL(subject):
+    t = ""
+    t = subject[subject.find("http://"):]
+    t = t[0:20]
+    if len(t) < 5:
+        return subject
+    else:
+        return subject.replace(t,'')    
+    
+def getCommand(subject):
+    t = ""
+    t = subject[subject.find("#RPiTTC"):]
+    #print len(t)
+    if len(t) < 5 :
+        return ""
+    else:
+        return t
+
+def parseCommand(command):
+    t = command[command.find(":")+1:]
+    commands = t.split(',')
+    return commands
